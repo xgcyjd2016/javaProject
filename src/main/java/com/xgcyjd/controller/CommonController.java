144e750beb67850a6a34dca625fe2a6b14e18455
zjlllll <1394877084@qq.com>
2018-12-03 10:41:13 +0800

    重写CommonController

--- src/main/java/com/xgcyjd/controller/CommonController.java
+++ src/main/java/com/xgcyjd/controller/CommonController.java
@@ -1,10 +1,15 @@
 package com.xgcyjd.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javafx.application.Application;
+import org.omg.CORBA.Request;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 
@@ -13,7 +18,9 @@ public class CommonController {
 
     @RequestMapping(value = "/logout")
     @ResponseBody
-    public HashMap<String,Object> logout(HttpSession session){
+    public HashMap<String,Object> logout(HttpServletRequest request){
+
+        HttpSession session = request.getSession();
 
         HashMap<String,Object> hashMap = new HashMap<>();
 
@@ -24,18 +31,20 @@ public class CommonController {
         return hashMap;
     }
 
-//    @RequestMapping(value = "/checkLogin")
-//    @ResponseBody
-//    public HashMap<String,Object> checkLogin(HttpSession session){
-//
-//        HashMap<String, Object> hashMap =new HashMap<>();
-//        if (session.getAttribute("manager")==null && session.getAttribute("user") ==null) {
-//            hashMap.put("state", 0);
-//            return hashMap;
-//        }else {
-//            hashMap.put("state",1);
-//            return hashMap;
-//        }
-//    }
+    @RequestMapping(value = "/checkLogin")
+    @ResponseBody
+    public HashMap<String,Object> checkLogin(HttpServletRequest request){
+
+        HttpSession session = request.getSession();
+
+        HashMap<String, Object> hashMap =new HashMap<>();
+        if (session.getAttribute("manager")==null && session.getAttribute("user") ==null) {
+            hashMap.put("state", 0);
+            return hashMap;
+        }else {
+            hashMap.put("state",1);
+            return hashMap;
+        }
+    }
 
 }
